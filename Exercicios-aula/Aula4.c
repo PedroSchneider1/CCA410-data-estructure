d1ee342141b9679dbdcda15574038b3909c1557e
Pedro Schneider <162492569+PedroSchneider1@users.noreply.github.com>
2025-09-02 22:03:56 -0300

    Salvo em 02-09-2025 22:03:56

--- Exercicios-aula/Aula4.c
+++ Exercicios-aula/Aula4.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+/*
+Especificações:
+    Função hash: h(k) = k mod 7
+    Tratamento de colisões: encadeamento externo
+    Conjunto: {190, 322, 172, 89, 13, 4, 769, 61, 15, 76}
+Tarefas:
+    1. Calcule h(k) para cada valor
+    2. Desenhe a tabela hash resultante
+    3. Simule inserção e remoção
+    4. Analise o fator de carga final
+*/
+
+#define TABLE_SIZE 10
+
+int h(int k){
+    return k % 7;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int hash[7];
+    int arr[] = {190, 322, 172, 89, 13, 4, 769, 61, 15, 76};
+    
+    for (size_t i = 0; i < 10; i++){
+        int index = h(arr[i]); // Calcula o índice usando a função de hash
+        printf("h(%3d) = %d\n", arr[i], index);
+
+        // Sondagem linear: procura a próxima posição livre
+        while (hash[index] != 0)
+        {
+            index++; // tenta a próxima posição
+            if (index >= TABLE_SIZE) // se passar do fim da tabela, volta ao início
+                index = 0;
+        }
+
+            // Insere o elemento na posição encontrada
+            hash[index] = arr[i];
+    }
+
     return 0;
 }
